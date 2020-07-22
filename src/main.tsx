6cf7597070da5541bda210d34ffcad1c66c42031
sprout2000 <office@nishigami.jp>
2020-07-22 10:24:45 +0900

    refactor: create main.tsx as the entry file

---
+++ src/main.tsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import App from './App';
+
+if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker
+      .register('./service-worker.js')
+      .then((registration) => {
+        console.log('SW registered: ', registration);
+      })
+      .catch((registrationError) => {
+        console.log('SW registration failed: ', registrationError);
+      });
+  });
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
