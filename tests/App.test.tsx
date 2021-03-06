ae4fe56046d175340d5a638047f573752dbbd042
sprout2000 <wassabie64@gmail.com>
2021-03-06 18:32:11 +0900

    perf: create App.test.tsx

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,11 +1,32 @@
 import React from 'react';
 import '@testing-library/jest-dom';
-import { render } from '@testing-library/react';
+import { render, screen } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 
 import App from '../src/Components/App';
 
 describe('App component', () => {
   test('render App component', () => {
     render(<App />);
+
+    expect(screen.getByTestId(/title/)).toHaveTextContent('すべてのタスク');
+
+    userEvent.click(screen.getByRole('button', { name: /menu/ }));
+    expect(screen.getByText(/ごみ箱/)).toBeInTheDocument();
+
+    userEvent.click(screen.getByRole('button', { name: /すべてのタスク/ }));
+    expect(screen.getByTestId(/title/)).toHaveTextContent('すべてのタスク');
+
+    userEvent.click(screen.getByRole('button', { name: /menu/ }));
+    userEvent.click(screen.getByRole('button', { name: /マイタスク/ }));
+    expect(screen.getByTestId(/title/)).toHaveTextContent('マイタスク');
+
+    userEvent.click(screen.getByRole('button', { name: /menu/ }));
+    userEvent.click(screen.getByRole('button', { name: /完了したタスク/ }));
+    expect(screen.getByTestId(/title/)).toHaveTextContent('完了したタスク');
+
+    userEvent.click(screen.getByRole('button', { name: /menu/ }));
+    userEvent.click(screen.getByRole('button', { name: /ごみ箱/ }));
+    expect(screen.getByTestId(/title/)).toHaveTextContent('ごみ箱');
   });
 });
