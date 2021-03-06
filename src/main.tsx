351dd6f2b6cc746c683417b21e9cfbf9cd440a81
sprout2000 <wassabie64@gmail.com>
2021-03-06 17:16:52 +0900

    fix: add typeface-roboto

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 
+import 'typeface-roboto';
 import App from './Components/App';
 
 if (process.env.NODE_ENV === 'production' && 'serviceWorker' in navigator) {
