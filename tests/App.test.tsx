d647be478aaecbe0ae791fb0183111b06c90c80d
sprout2000 <wassabie64@gmail.com>
2021-03-07 09:40:58 +0900

    chore: update App.test.tsx

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -13,20 +13,33 @@ describe('App component', () => {
 
     userEvent.click(screen.getByRole('button', { name: /menu/ }));
     expect(screen.getByText(/ごみ箱/)).toBeInTheDocument();
+    userEvent.click(screen.getAllByRole('presentation')[0]);
 
     userEvent.click(screen.getByRole('button', { name: /すべてのタスク/ }));
     expect(screen.getByTestId(/title/)).toHaveTextContent('すべてのタスク');
+    expect(
+      screen.getByRole('button', { name: 'add-button' })
+    ).toBeInTheDocument();
 
     userEvent.click(screen.getByRole('button', { name: /menu/ }));
     userEvent.click(screen.getByRole('button', { name: /マイタスク/ }));
     expect(screen.getByTestId(/title/)).toHaveTextContent('マイタスク');
+    expect(
+      screen.getByRole('button', { name: 'add-button' })
+    ).toBeInTheDocument();
 
     userEvent.click(screen.getByRole('button', { name: /menu/ }));
     userEvent.click(screen.getByRole('button', { name: /完了したタスク/ }));
     expect(screen.getByTestId(/title/)).toHaveTextContent('完了したタスク');
+    expect(screen.getByRole('button', { name: 'add-button' })).toHaveAttribute(
+      'disabled'
+    );
 
     userEvent.click(screen.getByRole('button', { name: /menu/ }));
     userEvent.click(screen.getByRole('button', { name: /ごみ箱/ }));
     expect(screen.getByTestId(/title/)).toHaveTextContent('ごみ箱');
+    expect(
+      screen.getByRole('button', { name: 'delete-button' })
+    ).toBeInTheDocument();
   });
 });
