5e54490e222f7a274489e8ca2a2ed2c960fca023
sprout2000 <wassabie64@gmail.com>
2021-03-10 10:00:44 +0900

    chore: update SideBar.test.tsx

--- tests/SideBar.test.tsx
+++ tests/SideBar.test.tsx
@@ -9,12 +9,14 @@ describe('SideBar component', () => {
   test('render SideBar component', () => {
     const onToggle = jest.fn();
     const onSort = jest.fn();
+    const onQR = jest.fn();
 
     render(
       <SideBar
         drawerOpen={true}
         toggleDrawer={onToggle}
         handleOnSort={onSort}
+        onQROpen={onQR}
       />
     );
 
@@ -22,11 +24,13 @@ describe('SideBar component', () => {
     const todo = screen.getByRole('button', { name: 'マイタスク' });
     const done = screen.getByRole('button', { name: '完了したタスク' });
     const trash = screen.getByRole('button', { name: 'ごみ箱' });
+    const share = screen.getByRole('button', { name: 'このアプリを共有' });
 
     expect(all).toBeInTheDocument();
     expect(todo).toBeInTheDocument();
     expect(done).toBeInTheDocument();
     expect(trash).toBeInTheDocument();
+    expect(share).toBeInTheDocument();
 
     userEvent.click(all);
     expect(onSort).toBeCalledTimes(1);
@@ -44,7 +48,11 @@ describe('SideBar component', () => {
     expect(onSort).toBeCalledTimes(4);
     expect(onToggle).toBeCalledTimes(4);
 
+    userEvent.click(share);
+    expect(onQR).toBeCalledTimes(1);
+    expect(onToggle).toBeCalledTimes(5);
+
     userEvent.type(screen.getByRole('list'), `${specialChars.escape}`);
-    expect(onToggle).toBeCalledTimes(6);
+    expect(onToggle).toBeCalledTimes(7);
   });
 });
