9d3309bb9b779300f173cb1ff7e00fbc81fa0107
sprout2000 <wassabie64@gmail.com>
2021-03-10 09:57:01 +0900

    chore: update App.test.tsx

--- tests/App.test.tsx
+++ tests/App.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import 'jest-canvas-mock';
 import '@testing-library/jest-dom';
 import { render, screen } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
