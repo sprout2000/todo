1f6e4b0777aafcab6c9038a71c722ad495841c5a
sprout2000 <wassabie64@gmail.com>
2023-02-24 08:56:01 +0900

    chap.05

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,16 @@
+import { useState } from 'react';
+
 export const App = () => {
+  const [text, setText] = useState('');
+
   return (
     <div>
       <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
+        <input
+          type="text"
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        />
         <input
           type="submit"
           value="追加"
