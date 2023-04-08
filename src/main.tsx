aa08ce9495462f148f241885b44636a205732369
sprout2000 <wassabie64@gmail.com>
2023-04-08 13:21:16 +0900

    chap.29

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,13 @@
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
+import { registerSW } from 'virtual:pwa-register';
 
 import { App } from './App';
 
-const root = createRoot(document.getElementById('root') as Element);
-
-root.render(
+createRoot(document.getElementById('root') as Element).render(
   <StrictMode>
     <App />
   </StrictMode>
 );
+
+registerSW();
