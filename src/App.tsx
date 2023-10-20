87e8d16c39caf61b258ab73c55f63a774d9c7bf8
sprout2000 <wassabie64@gmail.com>
2023-10-21 08:36:52 +0900

    chap.07

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,38 @@
+import { useState } from "react";
+
+type Todo = {
+  value: string;
+};
+
 export const App = () => {
+  const [text, setText] = useState("");
+  const [todos, setTodos] = useState<Todo[]>([]);
+
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setText(e.target.value);
+  };
+
+  const handleSubmit = () => {
+    if (!text) return;
+
+    const newTodo: Todo = {
+      value: text,
+    };
+
+    setTodos((todos) => [newTodo, ...todos]);
+    setText("");
+  };
+
   return (
     <div>
-      <form onSubmit={(e) => e.preventDefault()}>
-        <input type="text" value="" onChange={(e) => e.preventDefault()} />
-        <input
-          type="submit"
-          value="追加"
-          onSubmit={(e) => e.preventDefault()}
-        />
+      <form
+        onSubmit={(e) => {
+          e.preventDefault();
+          handleSubmit();
+        }}
+      >
+        <input type="text" value={text} onChange={(e) => handleChange(e)} />
+        <input type="submit" value="追加" onSubmit={handleSubmit} />
       </form>
     </div>
   );
