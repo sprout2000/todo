81c0efc1cb3984e56d07318148eda3d9bfe09c33
sprout2000 <wassabie64@gmail.com>
2023-10-21 09:02:05 +0900

    chap.23

--- src/FormDialog.tsx
+++ src/FormDialog.tsx
@@ -1,23 +1,50 @@
+import Button from "@mui/material/Button";
+import Dialog from "@mui/material/Dialog";
+import TextField from "@mui/material/TextField";
+import DialogActions from "@mui/material/DialogActions";
+
 type Props = {
   text: string;
+  dialogOpen: boolean;
   onSubmit: () => void;
-  onChange: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  onChange: (
+    e: React.ChangeEvent<HTMLInputElement | HTMLTextAreaElement>
+  ) => void;
+  onToggleDialog: () => void;
 };
 
-export const FormDialog = (props: Props) => {
-  return (
+export const FormDialog = (props: Props) => (
+  <Dialog fullWidth open={props.dialogOpen} onClose={props.onToggleDialog}>
     <form
       onSubmit={(e) => {
         e.preventDefault();
         props.onSubmit();
       }}
     >
-      <input
-        type="text"
-        value={props.text}
-        onChange={(e) => props.onChange(e)}
-      />
-      <input type="submit" value="追加" onSubmit={props.onSubmit} />
+      <div style={{ margin: "1em" }}>
+        <TextField
+          aria-label="todo-input"
+          variant="standard"
+          style={{
+            width: "100%",
+            fontSize: "16px",
+            fontFamily: "-apple-system, BlinkMacSystemFont, Roboto, sans-serif",
+          }}
+          label="タスクを入力..."
+          onChange={(e) => props.onChange(e)}
+          value={props.text}
+          autoFocus
+        />
+        <DialogActions>
+          <Button
+            aria-label="form-add"
+            color="secondary"
+            onClick={props.onSubmit}
+          >
+            追加
+          </Button>
+        </DialogActions>
+      </div>
     </form>
-  );
-};
+  </Dialog>
+);
