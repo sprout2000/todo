f9715a64d0d81cb3c4ef0c1b90787aac3ccafe6f
sprout2000 <wassabie64@gmail.com>
2023-10-21 09:12:35 +0900

    chap.28

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,40 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import { VitePWA } from "vite-plugin-pwa";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    VitePWA({
+      manifest: {
+        name: "Todo App (PWA)",
+        short_name: "Todo",
+        description: "Todo プログレッシブ・ウェブアプリ",
+        start_url: ".",
+        display: "standalone",
+        orientation: "portrait",
+        theme_color: "#3f51b2",
+        background_color: "#efeff4",
+        icons: [
+          {
+            src: "icon-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+          {
+            src: "icon-512x512-mask.png",
+            sizes: "512x512",
+            type: "image/png",
+            purpose: "maskable",
+          },
+        ],
+      },
+    }),
+  ],
 });
